fcdb0d7efb8aac7111d81e71e7b5931780da0ca6
LuanCod <luan_developer@outlook.com>
2024-05-08 19:02:45 -0300

    Adicionar arquivos de projeto.

---
+++ cadastro-materia-prima/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cadastro_materia_prima.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
