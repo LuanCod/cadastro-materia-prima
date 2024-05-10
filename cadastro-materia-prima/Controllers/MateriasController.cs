7cdaa9c7e19d60c9ba858dc6765350951d1a2bf0
LuanCod <luan_developer@outlook.com>
2024-05-10 20:44:56 -0300

    Crud Matéria-prima

---
+++ cadastro-materia-prima/Controllers/MateriasController.cs
@@ -0,0 +1,121 @@
+﻿using cadastro_materia_prima.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace cadastro_materia_prima.Controllers
+{
+    public class MateriasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MateriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Materias.ToListAsync();
+            
+            return View(dados);
+        }
+
+        public IActionResult Create()
+        {
+        return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Materia materia)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Materias.Add(materia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            
+            }
+            return View(materia);
+        }
+
+        public async Task<IActionResult> Edit(int? id)            
+        {
+            if(id == null)
+                return NotFound();
+
+            var dados = await _context.Materias.FindAsync(id);
+
+            if(dados == null)
+                return NotFound();
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Materia materia)
+        {
+            if(id != materia.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Materias.Update(materia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+
+
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {  
+            if(id == null)
+                return NotFound();         
+            
+            var dados = await _context.Materias.FindAsync(id);
+            
+            if (dados == null)
+                return NotFound();
+
+
+            return View(dados);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Materias.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+
+            return View(dados);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Materias.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            _context.Materias.Remove(dados);
+            await _context.SaveChangesAsync();
+
+
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
--- cadastro-materia-prima/Models/materia.cs
+++ cadastro-materia-prima/Models/materia.cs
@@ -10,39 +10,49 @@ namespace cadastro_materia_prima.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o nome!")]
-        public string? Nome { get; set; }
+        public string Nome { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o tipo!")]
-        public string? Tipo { get; set;}
+        public string Tipo { get; set;}
 
         [Required(ErrorMessage = "Obrigatório informar o valor energético!")]
+        [Display(Name = "Valor\nEnergético")]
+
         public double ValorEnergetico { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar os carboidratos!")]
         public double Carboidratos { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar os açucares totais!")]
+        [Display(Name = "Açucares\nTotais")]
         public double AcucaresTotais { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar os açucares adicionados!")]
+        [Display(Name = "Açucares\nAdicionados")]
         public double AcucaresAdicionados { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar as proteínas!")]
+        [Display(Name = "Proteínas")]
         public double Proteinas { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar as gorduras totais!")]
+        [Display(Name = "Gorduras\nTotais")]
         public double GordurasTotais { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar as gorduras saturadas!")]
+        [Display(Name = "Gorduras\nSaturadas")]
         public double GordurasSaturadas { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar as gorduras trans!")]
+        [Display(Name = "Gorduras\nTrans")]
         public double GordurasTrans { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar a fibra alimentar!")]
+        [Display(Name = "Fibra\nAlimentar")]
         public double FibraAlimentar { get; set; }
 
         [Required(ErrorMessage = "Obrigatório informar o sódio!")]
+        [Display(Name = "Sódio")]
         public double Sodio { get; set; }
     }
 }
